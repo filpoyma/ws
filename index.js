ba8c890b8307cd5c952355663b0c819c87e4189b
rom <filpoyma@gmail.com>
2022-11-28 08:37:22 +0300

    master

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// @ts-nocheck
 const express = require('express');
-const ws = require('ws');
 
 const app = express();
 
@@ -18,37 +16,3 @@ const port = process.env.PORT ?? 3000;
 const httpServer = app.listen(port, () => {
   console.log('Server started at: http://localhost:%s', port);
 });
-
-const wsServer = new ws.WebSocketServer({
-  server: httpServer,
-});
-
-// // EventEmitter  Эхо сервер для одного клиента
-wsServer.on('connection', (client) => { // on('connection') - когда присоединился клиент
-  console.log('>>>> client connected <<<<<');
-  client.on('message', (data) => { // client.on('message') - дожидаемся от клиента сообщений (прилетает дата)
-    const message = data.toString()
-    console.log('>>client send message<<', message);
-    client.send(message); // отправляет сообщение обратоно
-  });
-});
-
-
-// EventEmitter Эхо сервер для многих клиентов
-// wsServer.on('connection', (currentClient) => {
-//   console.log('>>>> client connected. clients: ',  wsServer.clients.size); // wsServer.clients -  итерируемый обьект Set
-//   // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Set
-//
-//   currentClient.on('message', (data) => {
-//     const message = JSON.parse(data);
-//     console.log('>>> save to DataBase');
-//     console.log('>>> send messages to all clients', message);
-//     wsServer.clients.forEach((client) => {
-//       client.send(JSON.stringify(message));
-//     });
-//   });
-//
-//   currentClient.on('close', (client) => {
-//     console.log('>>>> client disconnected. clients: ',  wsServer.clients.size);
-//   });
-// });
